efbb68969d2918d92de7c9c977f640ca30d871e7
haz <hazpoldev@gmail.com>
2025-04-27 16:40:12 +0800

    Added New Function: Recently Modified Files (Last 24 Hours)

--- monitor.sh
+++ monitor.sh
@@ -14,3 +14,9 @@ echo
 echo "Disk Usage"
 df -h --total | awk '/total/{print "Total Disk: "$2", Used: "$3", Free: "$4}'
 echo
+
+# Recently Modified Files (Last 24 Hours)
+echo "Recently Modified Files (Last 24 Hours)"
+if command -v find >/dev/null 2>&1; then
+    find ~ -type f -mtime -1 -print 2>/dev/null
+fi
